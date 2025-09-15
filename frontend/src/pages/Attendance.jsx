5cc4ebe82d1fdca559bca0b620ed939b36312283
GGGGGGGGGGGGGGG_J <115770858+NaturalSoda4552@users.noreply.github.com>
2025-09-16 01:06:55 +0900

    feat: 출석 페이지 생성 및 라우팅 세팅 (#7)
    * feat: 출석 페이지 생성 및 라우팅 세팅
    
    * refactor: 출석 페이지 중복 컴포넌트를 분리하여 중복 코드 제거

--- frontend/src/pages/Attendance.jsx
+++ frontend/src/pages/Attendance.jsx
@@ -1,5 +1,38 @@
+import { useNavigate } from 'react-router-dom';
+import './Attendance.css';
+import icon1 from '../assets/at_icon_1.png';
+import icon2 from '../assets/at_icon_2.png';
+import icon3 from '../assets/at_icon_3.png';
+import AttendanceSelectBox from '../components/\bAttendanceSelectBox.jsx';
+
+const attendanceSelectData = [
+  { icon: icon1, text: '전체 출석', path: '/attendance-all' },
+  { icon: icon2, text: '자산 운용 출석', path: '/attendance-asset' },
+  { icon: icon3, text: '금융 IT 출석', path: '/attendance-financeit' },
+];
+
 const Attendance = () => {
-  return <div>Attendance Page</div>;
+  const nav = useNavigate();
+
+  return (
+    <>
+      <div className="attendance-contanier">
+        <div className="attendance-text">출석하기</div>
+        <div className="attendance-list">
+          {attendanceSelectData.map((item, idx) => {
+            return (
+              <AttendanceSelectBox
+                key={idx}
+                icon={item.icon}
+                text={item.text}
+                onClick={() => nav(item.path)}
+              />
+            );
+          })}
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Attendance;
---
+++ frontend/src/pages/AttendanceAll.jsx
@@ -0,0 +1,5 @@
+const AttendanceAll = () => {
+  return <div>AttendanceAll Page</div>;
+};
+
+export default AttendanceAll;
