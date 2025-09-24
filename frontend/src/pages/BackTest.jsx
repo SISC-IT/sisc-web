0f1c8feb9941a87c4cbd00fe098206a569e4f963
gxuoo <rbdud189@naver.com>
2025-09-25 00:30:02 +0900

    FE-feat: 백테스팅 요소를 담을 container 추가(SISC1-158)

--- frontend/src/pages/BackTest.jsx
+++ frontend/src/pages/BackTest.jsx
@@ -1,5 +1,12 @@
+import styles from './BackTest.module.css';
+
 const BackTest = () => {
   return <div>BackTest Page</div>;
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.pageTitle}>백테스팅</h1>
+    </div>
+  );
 };
 
 export default BackTest;
