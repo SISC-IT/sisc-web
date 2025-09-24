ed216883353f528bc8b8bc5a7b77c777d78aa189
gxuoo <rbdud189@naver.com>
2025-09-25 00:32:09 +0900

    FE-feat: 주식 Card 구현 (SISC1-158)

--- frontend/src/pages/BackTest.jsx
+++ frontend/src/pages/BackTest.jsx
@@ -1,13 +1,14 @@
 import styles from './BackTest.module.css';
 import StrategyInfoCard from '../components/backtest/StrategyInfoCard';
+import StocksCard from '../components/backtest/StocksCard';
 
 const BackTest = () => {
-  return <div>BackTest Page</div>;
   return (
     <div className={styles.container}>
       <h1 className={styles.pageTitle}>백테스팅</h1>
 
       <StrategyInfoCard />
+      <StocksCard />
     </div>
   );
 };
