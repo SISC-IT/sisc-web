f1ef65c1682ae1e8a609561827a86dfc258af75e
Rosa <138095962+otter2023@users.noreply.github.com>
2025-09-28 10:36:17 +0900

    SISC1-83 [FEAT] 베팅 게임 생성 스케쥴러 구현 (#24)
    * SISC1-83 [FEAT] 베팅 게임 생성 스케쥴러 구현
    
    * SISC1-83 [FIX] 충돌 해결 과정에서의 오타 수정

--- backend/src/main/java/org/sejongisc/backend/common/exception/ErrorCode.java
+++ backend/src/main/java/org/sejongisc/backend/common/exception/ErrorCode.java
@@ -31,10 +31,15 @@ public enum ErrorCode {
   INVALID_ACCESS_TOKEN(HttpStatus.UNAUTHORIZED, "유효하지 않은 엑세스 토큰입니다."),
 
   // USER
+
   USER_NOT_FOUND(HttpStatus.NOT_FOUND, "유저를 찾을 수 없습니다."),
   DUPLICATE_EMAIL(HttpStatus.CONFLICT, "이미 가입된 이메일입니다."),
   DUPLICATE_PHONE(HttpStatus.CONFLICT, "이미 사용 중인 전화번호입니다."),
-  DUPLICATE_USER(HttpStatus.CONFLICT, "이미 가입된 사용자입니다.");
+  DUPLICATE_USER(HttpStatus.CONFLICT, "이미 가입된 사용자입니다."),
+  
+   // BETTING
+
+  STOCK_NOT_FOUND(HttpStatus.NOT_FOUND, "주식 종목이 존재하지 않습니다.");
 
   private final HttpStatus status;
   private final String message;
