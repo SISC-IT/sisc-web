6fa329a3ac7789769d53fe3945bf83b17fc5eeef
wiggle01 <weedsingreenhouse@gmail.com>
2025-10-02 16:29:17 +0900

    sprint2 complete
    post CRUD and search implement

--- backend/src/main/java/org/sejongisc/backend/board/dao/PostRepository.java
+++ backend/src/main/java/org/sejongisc/backend/board/dao/PostRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 import java.util.UUID;
 
+
 public interface PostRepository extends JpaRepository<Post, UUID> {
     List<Post> findByTitleContainingOrContentContaining(String title, String content);
 }
