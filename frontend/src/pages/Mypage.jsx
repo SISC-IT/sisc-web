3874dc424506017392faa3f90789046c01a02792
gxuoo <rbdud189@naver.com>
2025-10-02 15:50:15 +0900

    [FE] SISC1-167 [FEAT]: 마이페이지에 프로필과 메뉴 컴포넌트 import

--- frontend/src/pages/Mypage.jsx
+++ frontend/src/pages/Mypage.jsx
@@ -1,5 +1,16 @@
+import styles from './Mypage.module.css';
+import MyPageMenu from '../components/mypage/MyPageMenu';
+import ProfileCard from '../components/mypage/ProfileCard';
+
 const Mypage = () => {
-  return <div>Mypage</div>;
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}>마이페이지</h1>
+
+      <ProfileCard />
+      <MyPageMenu />
+    </div>
+  );
 };
 
 export default Mypage;
