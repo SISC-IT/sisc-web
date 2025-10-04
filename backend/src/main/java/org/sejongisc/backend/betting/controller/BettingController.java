7fd376759756c93fb8afb82b9531e786b61893c4
Runea <otter2023@naver.com>
2025-10-04 14:01:36 +0900

    [BE] SISC1-84 [REFACTOR] : BetRound 조회 API를 PathVariable로 통합

--- backend/src/main/java/org/sejongisc/backend/betting/controller/BettingController.java
+++ backend/src/main/java/org/sejongisc/backend/betting/controller/BettingController.java
@@ -1,12 +1,15 @@
 package org.sejongisc.backend.betting.controller;
 
 
+import jakarta.validation.constraints.Pattern;
 import lombok.RequiredArgsConstructor;
 import org.sejongisc.backend.betting.entity.BetRound;
 import org.sejongisc.backend.betting.entity.Scope;
 import org.sejongisc.backend.betting.service.BettingService;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,30 +18,26 @@ import java.util.Optional;
 
 @RestController
 @RequiredArgsConstructor
+@Validated
 @RequestMapping("/api")
 public class BettingController {
 
     private final BettingService bettingService;
 
-    @GetMapping("bet-round/daily")
-    public ResponseEntity<BetRound> getTodayDailyBetRound(){
-        Optional<BetRound> betRound = bettingService.getActiveRound(Scope.DAILY);
+    @GetMapping("/bet-round/{scope}")
+    public ResponseEntity<BetRound> getTodayBetRound(
+            @PathVariable @Pattern(regexp = "daily|weekly") String scope){
+
+        Scope scopeEnum = Scope.valueOf(scope.toUpperCase());
+
+        Optional<BetRound> betRound = bettingService.getActiveRound(scopeEnum);
 
         return betRound
                 .map(ResponseEntity::ok)
                 .orElseGet(() -> ResponseEntity.notFound().build());
     }
 
-    @GetMapping("bet-round/weekly")
-    public ResponseEntity<BetRound> getTodayWeeklyBetRound(){
-        Optional<BetRound> betRound = bettingService.getActiveRound(Scope.WEEKLY);
-
-        return betRound
-                .map(ResponseEntity::ok)
-                .orElseGet(() -> ResponseEntity.notFound().build());
-    }
-
-    @GetMapping("bet-round/history")
+    @GetMapping("/bet-round/history")
     public ResponseEntity<List<BetRound>> getAllBetRounds(){
         List<BetRound> betRounds = bettingService.getAllBetRounds();
 
