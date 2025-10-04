0f3f0edec7053a41b10040022a13a2b9df21ee58
gxuoo <rbdud189@naver.com>
2025-10-04 20:51:44 +0900

    [FE] SISC1-167 [FEAT]: 포인트 내역 컴포넌트 구현

---
+++ frontend/src/components/mypage/PointsSection.jsx
@@ -0,0 +1,33 @@
+import styles from './Modal.module.css';
+import PointIcon from '../../assets/coin4.svg';
+
+function PointsSection({ items }) {
+  return (
+    <ul className={styles.list}>
+      {items.map((it) => (
+        <li key={it.id} className={styles.row}>
+          <div className={styles.left}>
+            <div className={styles.title}>{it.content}</div>
+            <div className={styles.time}>{it.time}</div>
+          </div>
+          <div className={styles.right}>
+            <img
+              src={PointIcon}
+              alt=""
+              aria-hidden="true"
+              className={styles.pointIcon}
+            />
+
+            <span
+              className={it.point >= 0 ? styles.pointPlus : styles.pointMinus}
+            >
+              {it.point > 0 ? `+${it.point}` : it.point}P
+            </span>
+          </div>
+        </li>
+      ))}
+    </ul>
+  );
+}
+
+export default PointsSection;
