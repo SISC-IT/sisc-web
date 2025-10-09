526847a2bb18766e7bba213b0238492aa25d72dd
Sohee <msciki790@gmail.com>
2025-10-10 01:39:06 +0900

    [BE] SISC1-59 [FEAT] 테스트 코드 수정

--- backend/src/test/java/org/sejongisc/backend/BackendApplicationTests.java
+++ backend/src/test/java/org/sejongisc/backend/BackendApplicationTests.java
@@ -7,17 +7,6 @@ import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
 @ActiveProfiles("test")
-@TestPropertySource(properties = {
-		"jwt.secret=test-secret",
-		"spring.datasource.url=jdbc:h2:mem:testdb",
-		"spring.datasource.username=sa",
-		"spring.datasource.password=",
-		"google.client.id=test-google-id",
-		"google.client.secret=test-google-secret",
-		"kakao.client.id=test-kakao-id",
-		"github.client.id=test-github-id",
-		"github.client.secret=test-github-secret"
-})
 class BackendApplicationTests {
 
 	@Test
