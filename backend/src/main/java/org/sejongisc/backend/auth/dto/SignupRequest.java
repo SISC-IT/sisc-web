f1177fb9b74092fdf02edaa07ac6a4dbc385b6ea
Sohee <msciki790@gmail.com>
2025-10-12 18:18:44 +0900

    [BE] SISC1-59 [FEAT] email pattern 수정

--- backend/src/main/java/org/sejongisc/backend/auth/dto/SignupRequest.java
+++ backend/src/main/java/org/sejongisc/backend/auth/dto/SignupRequest.java
@@ -17,7 +17,7 @@ public class SignupRequest {
     private String name;
 
     @NotBlank(message = "이메일은 필수입니다.")
-    @Pattern(regexp = "^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}$", message = "유효한 이메일 형식이 아닙니다.")
+    @Pattern(regexp = "^[A-Za-z0-9][A-Za-z0-9+_.'-]*[A-Za-z0-9]@[A-Za-z0-9]([A-Za-z0-9-]*[A-Za-z0-9])?(\\.[A-Za-z0-9]([A-Za-z0-9-]*[A-Za-z0-9])?)*\\.[A-Za-z]{2,}$", message = "유효한 이메일 형식이 아닙니다.")
     private String email;
 
     @NotBlank(message = "비밀번호는 필수입니다.")
