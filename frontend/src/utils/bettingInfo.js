229c2664c9a1b5dd6d73da7ef17449afbb53c16b
김동은 <ehddms0517@naver.com>
2025-10-16 00:05:09 +0900

    [FE] SISC1-181 [FIX]: 구분을 위해 다른 값 설정

--- frontend/src/utils/bettingInfo.js
+++ frontend/src/utils/bettingInfo.js
@@ -9,11 +9,11 @@ export const mockDailyBet = {
 };
 
 export const mockWeeklyBet = {
-  date: '2025-08-25',
+  date: '2025-08-18',
   symbol: 'AAPL',
-  closePrice: 96.3,
-  upperBet: 300,
+  closePrice: 98.5,
+  upperBet: 350,
   lowerBet: 300,
-  upBetCount: 200,
-  downBetCount: 200,
+  upBetCount: 150,
+  downBetCount: 230,
 };
