7df6f56e9b3bf5aad4b25d9232225ed7af099d1c
Wi Seungjae <132977754+discipline24@users.noreply.github.com>
2025-10-29 20:52:32 +0900

    [BE] SISC1-56 [FEAT] 외부 DB의 주식 정보 이용을 위한 멀티 데이터소스 구현

---
+++ backend/src/main/java/org/sejongisc/backend/stock/repository/PriceDataRepository.java
@@ -0,0 +1,14 @@
+package org.sejongisc.backend.stock.repository;
+
+import org.sejongisc.backend.stock.entity.PriceData;
+import org.sejongisc.backend.stock.entity.PriceDataId;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Repository
+public interface PriceDataRepository extends JpaRepository<PriceData, PriceDataId> {
+    List<PriceData> findByTickerAndDateBetweenOrderByDateAsc(String ticker, LocalDate startDate, LocalDate endDate);
+}
