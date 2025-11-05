a1298aa901f43e4a4e7e49b300fccca037aa629c
김동은 <ehddms0517@naver.com>
2025-11-06 00:37:19 +0900

    [FE] SISC1-198 [FEAT] 헤더 적용 및 토글 사이드바 전체 페이지 적용

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,37 @@
+import styles from './Header.module.css';
+import { useNavigate } from 'react-router-dom';
+import Logo from '../assets/logo.png';
+
+const Header = ({ onToggleSidebar, isOpen }) => {
+  const nav = useNavigate();
+  return (
+    <header className={styles.header}>
+      <div className={styles.left}>
+        <button
+          className={styles.menuButton}
+          onClick={onToggleSidebar}
+          aria-label={isOpen ? '메뉴 닫기' : '메뉴 열기'}
+          aria-expanded={isOpen}
+        >
+          <span></span>
+          <span></span>
+          <span></span>
+        </button>
+        <div className={styles.brand} onClick={() => nav('/')}>
+          <img className={styles.logo} src={Logo} alt="세종투자연구회 로고" />
+          <span className={styles.title}>세종투자연구회</span>
+        </div>
+      </div>
+      <div className={styles.right}>
+        <button className={styles.login} onClick={() => nav('/login')}>
+          로그인
+        </button>
+        <button className={styles.signUp} onClick={() => nav('signup')}>
+          회원가입
+        </button>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -1,181 +1,122 @@
-import { NavLink, useLocation, useNavigate } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import styles from './Sidebar.module.css';
-import Logo from '../assets/logo.png';
-import { useState } from 'react';
 
-const Sidebar = () => {
-  const location = useLocation();
-  const isRoot = location.pathname === '/';
-  const [isOpen, setIsOpen] = useState(false);
-  const toggleSidebar = () => setIsOpen(!isOpen);
-  const nav = useNavigate();
-
-  const menuSections = (
-    <>
-      <nav aria-label="사이드바">
-        <div className={styles['menu-section']}>
-          <span className={styles['menu-title']}>Main</span>
-          <ul>
-            <li>
-              <NavLink
-                to="/"
-                className={({ isActive }) =>
-                  isActive ? styles['active-link'] : styles['inactive-link']
-                }
-              >
-                홈
-              </NavLink>
-            </li>
-            <li>
-              <NavLink
-                to="/board"
-                className={({ isActive }) =>
-                  isActive ? styles['active-link'] : styles['inactive-link']
-                }
-              >
-                게시판
-              </NavLink>
-            </li>
-          </ul>
-        </div>
-
-        <div className={styles['menu-section']}>
-          <span className={styles['menu-title']}>출석체크</span>
-          <ul>
-            <li>
-              <NavLink
-                to="/attendance"
-                className={({ isActive }) =>
-                  isActive ? styles['active-link'] : styles['inactive-link']
-                }
-              >
-                출석하기
-              </NavLink>
-            </li>
-            <li>
-              <NavLink
-                to="/attendance-manage"
-                className={({ isActive }) =>
-                  isActive ? styles['active-link'] : styles['inactive-link']
-                }
-              >
-                출석관리(담당자)
-              </NavLink>
-            </li>
-          </ul>
-        </div>
-
-        <div className={styles['menu-section']}>
-          <span className={styles['menu-title']}>트레이딩</span>
-          <ul>
-            <li>
-              <NavLink
-                to="/quant-bot"
-                className={({ isActive }) =>
-                  isActive ? styles['active-link'] : styles['inactive-link']
-                }
-              >
-                퀀트봇
-              </NavLink>
-            </li>
-            <li>
-              <NavLink
-                to="/stock-game"
-                className={({ isActive }) =>
-                  isActive ? styles['active-link'] : styles['inactive-link']
-                }
-              >
-                주식베팅
-              </NavLink>
-            </li>
-            <li>
-              <NavLink
-                to="/back-test"
-                className={({ isActive }) =>
-                  isActive ? styles['active-link'] : styles['inactive-link']
-                }
-              >
-                백테스팅
-              </NavLink>
-            </li>
-          </ul>
-        </div>
-
-        <div className={styles['menu-section']}>
-          <span className={styles['menu-title']}>계정</span>
-          <ul>
-            <li>
-              <NavLink
-                to="/mypage"
-                className={({ isActive }) =>
-                  isActive ? styles['active-link'] : styles['inactive-link']
-                }
-              >
-                마이페이지
-              </NavLink>
-            </li>
-          </ul>
-        </div>
-      </nav>
-    </>
-  );
-
-  if (isRoot) {
-    return (
-      <div>
-        {/* 홈에서는 버튼만 고정 */}
-        <div className={styles.left}>
-          <button
-            className={styles.menuButton}
-            onClick={toggleSidebar}
-            aria-label={isOpen ? '메뉴 닫기' : '메뉴 열기'}
-            aria-expanded={isOpen}
-          >
-            <span></span>
-            <span></span>
-            <span></span>
-          </button>
-          <div className={styles.brand} onClick={() => nav('/')}>
-            <img className={styles.logo} src={Logo} alt="세종투자연구회 로고" />
-            <span className={styles.title}>세종투자연구회</span>
-          </div>
-        </div>
-
-        {/* 클릭 시 사이드바 슬라이드 */}
-        <div
-          className={`${styles.homeSidebarMenu} ${
-            isOpen ? styles.open : styles.closed
-          }`}
-          aria-hidden={!isOpen}
-        >
-          {menuSections}
-        </div>
-      </div>
-    );
-  }
-
-  // 홈 외 페이지는 항상 열려있음
+const Sidebar = ({ isOpen }) => {
   return (
-    <div className={styles.sidebar}>
-      <div className={styles['sidebar-header']}>
-        <div className={styles['sidebar-header-logo']}></div>
-        <div className={styles['sidebar-header-text']}>
-          <span className={styles['sidebar-header-text-ko']}>
-            세종투자연구회
-          </span>
-          <span className={styles['sidebar-header-text-en']}>Finance . IT</span>
-        </div>
+    <div>
+      {/* 클릭 시 사이드바 슬라이드 */}
+      <div
+        className={`${styles.homeSidebarMenu} ${
+          isOpen ? styles.open : styles.closed
+        }`}
+        aria-hidden={!isOpen}
+      >
+        <nav aria-label="사이드바">
+          <div className={styles['menu-section']}>
+            <span className={styles['menu-title']}>Main</span>
+            <ul>
+              <li>
+                <NavLink
+                  to="/"
+                  className={({ isActive }) =>
+                    isActive ? styles['active-link'] : styles['inactive-link']
+                  }
+                >
+                  홈
+                </NavLink>
+              </li>
+              <li>
+                <NavLink
+                  to="/board"
+                  className={({ isActive }) =>
+                    isActive ? styles['active-link'] : styles['inactive-link']
+                  }
+                >
+                  게시판
+                </NavLink>
+              </li>
+            </ul>
+          </div>
+
+          <div className={styles['menu-section']}>
+            <span className={styles['menu-title']}>출석체크</span>
+            <ul>
+              <li>
+                <NavLink
+                  to="/attendance"
+                  className={({ isActive }) =>
+                    isActive ? styles['active-link'] : styles['inactive-link']
+                  }
+                >
+                  출석하기
+                </NavLink>
+              </li>
+              <li>
+                <NavLink
+                  to="/attendance-manage"
+                  className={({ isActive }) =>
+                    isActive ? styles['active-link'] : styles['inactive-link']
+                  }
+                >
+                  출석관리(담당자)
+                </NavLink>
+              </li>
+            </ul>
+          </div>
+
+          <div className={styles['menu-section']}>
+            <span className={styles['menu-title']}>트레이딩</span>
+            <ul>
+              <li>
+                <NavLink
+                  to="/quant-bot"
+                  className={({ isActive }) =>
+                    isActive ? styles['active-link'] : styles['inactive-link']
+                  }
+                >
+                  퀀트봇
+                </NavLink>
+              </li>
+              <li>
+                <NavLink
+                  to="/stock-game"
+                  className={({ isActive }) =>
+                    isActive ? styles['active-link'] : styles['inactive-link']
+                  }
+                >
+                  주식베팅
+                </NavLink>
+              </li>
+              <li>
+                <NavLink
+                  to="/back-test"
+                  className={({ isActive }) =>
+                    isActive ? styles['active-link'] : styles['inactive-link']
+                  }
+                >
+                  백테스팅
+                </NavLink>
+              </li>
+            </ul>
+          </div>
+
+          <div className={styles['menu-section']}>
+            <span className={styles['menu-title']}>계정</span>
+            <ul>
+              <li>
+                <NavLink
+                  to="/mypage"
+                  className={({ isActive }) =>
+                    isActive ? styles['active-link'] : styles['inactive-link']
+                  }
+                >
+                  마이페이지
+                </NavLink>
+              </li>
+            </ul>
+          </div>
+        </nav>
       </div>
-      <hr
-        style={{
-          width: '163px',
-          marginBottom: '30px',
-          border: 'none',
-          height: '1px',
-          backgroundColor: '#656565',
-        }}
-      />
-      {menuSections}
     </div>
   );
 };
