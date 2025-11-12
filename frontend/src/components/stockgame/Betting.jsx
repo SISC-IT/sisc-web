18d028fb7cd0a72b749b28459e20c93e63ceac4e
김동은 <86216821+DongEun02@users.noreply.github.com>
2025-11-12 20:56:51 +0900

    [FE] SISC1-77 [FEAT] : 베팅 취소 및 페이지네이션 (#94)
    * [FE] SISC1-77 [FEAT] : 베팅 표시 및 베팅 취소
    
    * [FE] SISC1-77 [FEAT]: 베팅 이력 페이지네이션

--- frontend/src/components/stockgame/Betting.jsx
+++ frontend/src/components/stockgame/Betting.jsx
@@ -2,9 +2,33 @@ import styles from './Betting.module.css';
 import icon1 from '../../assets/at_icon_1.png';
 import StockInfoItem from './StockInfoItem';
 import { mockDailyBet, mockWeeklyBet } from '../../utils/bettingInfo';
+import { useState } from 'react';
 
 const DailyBetting = ({ period }) => {
+  const [isBetting, setIsBetting] = useState('none');
   const mockBettingInfo = period === 'daily' ? mockDailyBet : mockWeeklyBet;
+
+  const onClickUpBet = () => {
+    if (isBetting !== 'none') {
+      alert('이미 베팅하셨습니다.');
+    } else if (confirm('상승에 베팅하시겠습니까?')) {
+      setIsBetting('up');
+    }
+  };
+
+  const onClickDownBet = () => {
+    if (isBetting !== 'none') {
+      alert('이미 베팅하셨습니다.');
+    } else if (confirm('하락에 베팅하시겠습니까?')) {
+      setIsBetting('down');
+    }
+  };
+
+  const onClickCancelBet = () => {
+    if (confirm('베팅을 취소하시겠습니까?')) {
+      setIsBetting('none');
+    }
+  };
 
   const formatDate = (dateStr) => {
     const [year, month, day] = dateStr.split('-');
@@ -30,7 +54,13 @@ const DailyBetting = ({ period }) => {
             </div>
             <div className={styles['divider']} />
             <span>{mockBettingInfo.upBetCount}명</span>
-            <button className={styles['up-button']}>상승 ↑</button>
+            <button
+              className={`${styles['up-button']} ${isBetting === 'up' && styles.upActive}`}
+              onClick={onClickUpBet}
+            >
+              상승 ↑
+              {isBetting === 'up' && <div className={styles.upBetCheck}>v</div>}
+            </button>
           </div>
           {/* 하락 베팅 */}
           <div className={styles['lower-bet']}>
@@ -40,9 +70,22 @@ const DailyBetting = ({ period }) => {
             </div>
             <div className={styles['divider']} />
             <span>{mockBettingInfo.downBetCount}명</span>
-            <button className={styles['down-button']}>하락 ↓</button>
+            <button
+              className={`${styles['down-button']} ${isBetting === 'down' && styles.downActive}`}
+              onClick={onClickDownBet}
+            >
+              하락 ↓
+              {isBetting === 'down' && (
+                <div className={styles.downBetCheck}>v</div>
+              )}
+            </button>
           </div>
         </div>
+        {isBetting !== 'none' && (
+          <span className={styles.cancel} onClick={onClickCancelBet}>
+            X 취소하기
+          </span>
+        )}
       </div>
     </div>
   );
