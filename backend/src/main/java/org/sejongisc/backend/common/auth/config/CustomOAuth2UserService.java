0cdcfc8a5db255e7703ca4e0a8d3448f51c74034
Kosw6 <121417885+Kosw6@users.noreply.github.com>
2025-11-15 19:08:21 +0900

    Update CustomOAuth2UserService.java

--- backend/src/main/java/org/sejongisc/backend/common/auth/config/CustomOAuth2UserService.java
+++ backend/src/main/java/org/sejongisc/backend/common/auth/config/CustomOAuth2UserService.java
@@ -35,7 +35,7 @@ public class CustomOAuth2UserService implements OAuth2UserService <OAuth2UserReq
     @Override
     public OAuth2User loadUser(OAuth2UserRequest req) throws OAuth2AuthenticationException {
 
-      //  log.info("[CustomOAuth2UserService] loadUser START");
+        log.info("[CustomOAuth2UserService] loadUser START");
 
         OAuth2UserService<OAuth2UserRequest, OAuth2User> delegate =
                 new DefaultOAuth2UserService();
@@ -102,7 +102,7 @@ public class CustomOAuth2UserService implements OAuth2UserService <OAuth2UserReq
                     return saved;
                 });
 
-        // log.info("[CustomOAuth2UserService] User resolved → returning OAuth2User");
+        log.info("[CustomOAuth2UserService] User resolved → returning OAuth2User");
 
         Map<String, Object> attributes = new java.util.HashMap<>();
         attributes.put("provider", provider);           // google / kakao / github
