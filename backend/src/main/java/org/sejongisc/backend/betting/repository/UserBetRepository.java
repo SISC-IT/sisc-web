74aa7516b08953d09a1d76f485334ffd938883d9
20011744 <141496725+cksdid202@users.noreply.github.com>
2025-11-24 13:40:16 +0900

    fix: import 수정

--- backend/src/main/java/org/sejongisc/backend/betting/repository/UserBetRepository.java
+++ backend/src/main/java/org/sejongisc/backend/betting/repository/UserBetRepository.java
@@ -1,12 +1,12 @@
 package org.sejongisc.backend.betting.repository;
 
-import io.lettuce.core.dynamic.annotation.Param;
 import org.sejongisc.backend.betting.entity.BetRound;
 import org.sejongisc.backend.betting.entity.BetStatus;
 import org.sejongisc.backend.betting.entity.UserBet;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
