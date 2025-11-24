2ab1111ece179de7bb7f33e199251333781eb66c
gxuoo <rbdud189@naver.com>
2025-11-24 21:05:16 +0900

    [FE] SISC1-191 [FIX] : 필요없는 스타일 삭제

--- frontend/src/components/Layout.jsx
+++ frontend/src/components/Layout.jsx
@@ -13,7 +13,7 @@ function Layout() {
       <Header
         isRoot={isRoot}
         onToggleSidebar={() => setIsSidebarOpen(!isSidebarOpen)}
-        style={{ position: 'fixed', top: 0, left: 0, right: 0 }}
+        style={{ position: 'fixed' }}
       />
 
       <main
