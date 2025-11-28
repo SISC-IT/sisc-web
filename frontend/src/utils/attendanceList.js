6b8b2f091b6658e2dc88eb11bc779ebbd938fd3d
김동은 <ehddms0517@naver.com>
2025-11-29 02:24:47 +0900

    [FE] SISC1-226 [FEAT] : 출석 페이지 UI 구현

---
+++ frontend/src/utils/attendanceList.js
@@ -0,0 +1,37 @@
+export const attendanceList = [
+  {
+    date: '25-09-01',
+    startTime: '18:00:00',
+    available: 30,
+    round: 1,
+    name: '김성원',
+  },
+  {
+    date: '25-09-08',
+    startTime: '18:00:00',
+    available: 10,
+    round: 2,
+    name: '김성원',
+  },
+  {
+    date: '25-09-15',
+    startTime: '18:00:00',
+    available: 20,
+    round: 3,
+    name: '김성원',
+  },
+  {
+    date: '25-09-22',
+    startTime: '18:00:00',
+    available: 30,
+    round: 4,
+    name: '김성원',
+  },
+  {
+    date: '25-09-29',
+    startTime: '18:00:00',
+    available: 30,
+    round: 5,
+    name: '김성원',
+  },
+];
