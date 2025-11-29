5fc6465f417e971ec72406228884dd72aa35ab5f
김동은 <ehddms0517@naver.com>
2025-11-29 16:20:54 +0900

    [FE] SISC1-227 [FEAT] : 월간 세투연 헤더 구현

---
+++ frontend/src/pages/external/MonthlyReport.jsx
@@ -0,0 +1,25 @@
+import styles from './MonthlyReport.module.css';
+import logo from '../../assets/logo.png';
+
+const MonthlyReport = () => {
+  return (
+    <div className={styles.page}>
+      <div className={styles.header}>
+        <div className={styles.logoSection}>
+          <img src={logo} alt="세종투자연구회" className={styles.logo} />
+          <span className={styles.logoName}>월간 세투연</span>
+        </div>
+        <h1 className={styles.title}>
+          매월 업데이트되는{' '}
+          <strong className={styles.strong}>세투연 콘텐츠</strong>를 <br /> 한
+          곳에 모았습니다.
+        </h1>
+        <h2 className={styles.subTitle}>
+          지난 한 달의 활동과 자료들을 아카이브 형식으로 정리했어요.
+        </h2>
+      </div>
+    </div>
+  );
+};
+
+export default MonthlyReport;
