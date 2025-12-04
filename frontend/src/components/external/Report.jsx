34dce53e60c379c49d512098a3d026eaf7c56975
김동은 <ehddms0517@naver.com>
2025-12-04 23:46:00 +0900

    [FE] SISC1-227 [FEAT] : 임시 리포트 이미지 추가

--- frontend/src/components/external/Report.jsx
+++ frontend/src/components/external/Report.jsx
@@ -1,5 +1,6 @@
 import styles from './Report.module.css';
 import { useNavigate } from 'react-router-dom';
+import reportCover from '../../assets/external/report/report-cover.png';
 
 const Report = () => {
   const nav = useNavigate();
@@ -8,6 +9,11 @@ const Report = () => {
       <div
         className={styles.card}
         onClick={() => nav('/main/monthly-report-detail')}
+        // API 연동 후 경로 변경 예정
+        style={{
+          background: `url(${reportCover}) center no-repeat`,
+          backgroundSize: 'contain',
+        }}
       ></div>
       <span className={styles.title}>
         세투연의 한 달 활동 기록과 주요 성과 요약
