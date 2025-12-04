00cf79e3fd65e01d02b0f2d23764860de46e5e24
김동은 <ehddms0517@naver.com>
2025-12-05 00:08:17 +0900

    [FE] SISC1-227 [FEAT] : 리포트 상세 페이지 헤더 구현

--- frontend/src/pages/external/MonthlyReportDetail.jsx
+++ frontend/src/pages/external/MonthlyReportDetail.jsx
@@ -1,9 +1,18 @@
 import styles from './MonthlyReportDetail.module.css';
+import logo from '../../assets/logo.png';
+import { useNavigate } from 'react-router-dom';
 
 const MonthlyReportDetail = () => {
+  const nav = useNavigate();
   return (
     <div className={styles.container}>
-      <header className={styles.header}></header>
+      <header
+        className={styles.header}
+        onClick={() => nav('/main/monthly-report')}
+      >
+        <img src={logo} alt="로고" className={styles.logo} />
+        <span className={styles.title}>월간 세투연</span>
+      </header>
       <div className={styles.content}></div>
     </div>
   );
