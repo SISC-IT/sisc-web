a570dfb139cf3b8232c1269ab194f952fba9165a
김동은 <ehddms0517@naver.com>
2025-12-21 02:21:53 +0900

    [FE] SISC1-216 [FEAT] : 임원진 소개 페이지 구현

--- frontend/src/pages/external/Leaders.jsx
+++ frontend/src/pages/external/Leaders.jsx
@@ -1,9 +1,13 @@
 import styles from './External.module.css';
 import Filter from '../../components/external/Filter';
+import MemberCard from '../../components/external/MemberCard';
+import { useState } from 'react';
+import { executivesByGeneration } from '../../utils/executiveByGeneration';
 
 const cohort = Array.from({ length: 24 }, (_, i) => `${24 - i}기`);
 
 const Leaders = () => {
+  const [selected, setSelected] = useState(cohort[0]);
   return (
     <div className={styles.container}>
       <div className={styles.header}>
@@ -11,7 +15,10 @@ const Leaders = () => {
         <hr className={styles.divider} />
       </div>
       <div className={styles.info}>
-        <Filter items={cohort} />
+        <div className={styles.filter}>
+          <Filter items={cohort} value={selected} onChange={setSelected} />
+        </div>
+        <MemberCard datas={executivesByGeneration[selected]} />
       </div>
     </div>
   );
