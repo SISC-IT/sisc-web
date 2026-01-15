469647a02f8bc5498dcb6e16647119219ac9dbfa
Yooonjeong <wkddbs3935@naver.com>
2026-01-15 21:05:50 +0900

    [BE] [FEAT] 재베팅 시 기존 레코드 재활용을 위해 exists를 find 메서드로 변경

--- backend/src/main/java/org/sejongisc/backend/betting/repository/UserBetRepository.java
+++ backend/src/main/java/org/sejongisc/backend/betting/repository/UserBetRepository.java
@@ -1,19 +1,15 @@
 package org.sejongisc.backend.betting.repository;
 
 import org.sejongisc.backend.betting.entity.BetRound;
-import org.sejongisc.backend.betting.entity.BetStatus;
 import org.sejongisc.backend.betting.entity.UserBet;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 public interface UserBetRepository extends JpaRepository<UserBet, UUID> {
-    boolean existsByRoundAndUserId(BetRound round, UUID userId);
+    Optional<UserBet> findByRoundAndUserId(BetRound round, UUID userId);
 
     Optional<UserBet> findByUserBetIdAndUserId(UUID userBetId, UUID userId);
 
