93aa12bc57f0488dabd7a063a165113dc2f130b9
Yooonjeong <wkddbs3935@naver.com>
2026-01-26 19:21:02 +0900

    [BE] [FEAT] 통계 직접 업데이트 시 flushAutomatically = true 옵션 추가 및 주석 업데이트

--- backend/src/main/java/org/sejongisc/backend/betting/repository/BetRoundRepository.java
+++ backend/src/main/java/org/sejongisc/backend/betting/repository/BetRoundRepository.java
@@ -21,32 +21,43 @@ public interface BetRoundRepository extends JpaRepository<BetRound, UUID> {
 
     List<BetRound> findByStatusFalseAndSettleAtIsNullAndLockAtLessThanEqual(LocalDateTime now);
 
-    // 상승(UP) 통계 원자적 업데이트
-    @Modifying
+    /**
+     * 상승(UP) 통계 원자적 업데이트
+     * - flushAutomatically: 해당 메서드 호출 전 변경사항을 DB에 flush -> 데이터 유실 방지
+     * - clearAutomatically: 업데이트 후 1차 캐시를 비움 -> 조회 시 데이터 정합성 문제 방지
+     *      -> 이후 필요하다면 DB의 최신값 조회 필요
+     */
+    @Modifying(flushAutomatically = true, clearAutomatically = true)
     @Query(
         "UPDATE BetRound b " +
         "SET b.upBetCount = b.upBetCount + 1, b.upTotalPoints = b.upTotalPoints + :points " +
         "WHERE b.betRoundID = :id")
     void incrementUpStats(@Param("id") UUID id, @Param("points") long points);
 
-    // 하락(DOWN) 통계 원자적 업데이트
-    @Modifying
+    /**
+     * 하락(DOWN) 통계 원자적 업데이트
+     */
+    @Modifying(flushAutomatically = true, clearAutomatically = true)
     @Query(
         "UPDATE BetRound b " +
         "SET b.downBetCount = b.downBetCount + 1, b.downTotalPoints = b.downTotalPoints + :points " +
         "WHERE b.betRoundID = :id")
     void incrementDownStats(@Param("id") UUID id, @Param("points") long points);
 
-    // 상승(UP) 통계 감소 (취소 시)
-    @Modifying
-    @Query("" +
+    /**
+     * 상승(UP) 통계 감소 (취소 시)
+     */
+    @Modifying(flushAutomatically = true, clearAutomatically = true)
+    @Query(
         "UPDATE BetRound b " +
         "SET b.upBetCount = b.upBetCount - 1, b.upTotalPoints = b.upTotalPoints - :points " +
         "WHERE b.betRoundID = :id")
     void decrementUpStats(@Param("id") UUID id, @Param("points") long points);
 
-    // 하락(DOWN) 통계 감소 (취소 시)
-    @Modifying
+    /**
+     * 하락(DOWN) 통계 감소 (취소 시)
+     */
+    @Modifying(flushAutomatically = true, clearAutomatically = true)
     @Query(
         "UPDATE BetRound b " +
         "SET b.downBetCount = b.downBetCount - 1, b.downTotalPoints = b.downTotalPoints - :points " +
