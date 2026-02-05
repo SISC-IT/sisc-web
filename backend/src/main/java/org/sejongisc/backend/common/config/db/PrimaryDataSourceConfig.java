61c87b7d97d1ef336e693e9ed6710f99319b3216
Wi Seungjae <132977754+discipline24@users.noreply.github.com>
2026-02-05 23:57:42 +0900

    fix : jwtProvider도 같은 작업 진행

--- backend/src/main/java/org/sejongisc/backend/common/config/db/PrimaryDataSourceConfig.java
+++ backend/src/main/java/org/sejongisc/backend/common/config/db/PrimaryDataSourceConfig.java
@@ -74,7 +74,7 @@ public class PrimaryDataSourceConfig {
         Map<String, Object> jpaProps = new HashMap<>();
         jpaProps.put("hibernate.dialect", "org.hibernate.dialect.PostgreSQLDialect");
 
-        jpaProps.put("hibernate.hbm2ddl.auto", "create");
+        jpaProps.put("hibernate.hbm2ddl.auto", "update");
 
         return builder
                 .dataSource(dataSource)
