30dfe89abd1286f7b848e33f5899f481589d0f46
yyunee <gimyunhe@gmail.com>
2026-02-13 22:33:18 +0900

    [FIX] setup localEnvFile

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,46 +1,51 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import { VitePWA } from 'vite-plugin-pwa';
 
-export default defineConfig({
-  plugins: [
-    react(),
-    // VitePWA({
-    //   registerType: 'autoUpdate',
-    //   includeAssets: ['favicon.svg', 'robots.txt', 'apple-touch-icon.png'],
-    //   manifest: {
-    //     name: 'My PWA App',
-    //     short_name: 'PWAApp',
-    //     description: 'React + Vite + PWA starter template',
-    //     theme_color: '#ffffff',
-    //     icons: [
-    //       {
-    //         src: 'pwa-192x192.png',
-    //         sizes: '192x192',
-    //         type: 'image/png',
-    //       },
-    //       {
-    //         src: 'pwa-512x512.png',
-    //         sizes: '512x512',
-    //         type: 'image/png',
-    //       },
-    //       {
-    //         src: 'pwa-512x512.png',
-    //         sizes: '512x512',
-    //         type: 'image/png',
-    //         purpose: 'any maskable',
-    //       },
-    //     ],
-    //   },
-    // }),
-  ],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://54.180.175.139:8080',
-        changeOrigin: true,
-        secure: false,
+export default defineConfig(({ mode }) => {
+  // 현재 모드(development 등)에 맞는 .env 파일을 로드합니다.
+  const env = loadEnv(mode, process.cwd(), '');
+  return {
+    plugins: [
+      react(),
+      // VitePWA({
+      //   registerType: 'autoUpdate',
+      //   includeAssets: ['favicon.svg', 'robots.txt', 'apple-touch-icon.png'],
+      //   manifest: {
+      //     name: 'My PWA App',
+      //     short_name: 'PWAApp',
+      //     description: 'React + Vite + PWA starter template',
+      //     theme_color: '#ffffff',
+      //     icons: [
+      //       {
+      //         src: 'pwa-192x192.png',
+      //         sizes: '192x192',
+      //         type: 'image/png',
+      //       },
+      //       {
+      //         src: 'pwa-512x512.png',
+      //         sizes: '512x512',
+      //         type: 'image/png',
+      //       },
+      //       {
+      //         src: 'pwa-512x512.png',
+      //         sizes: '512x512',
+      //         type: 'image/png',
+      //         purpose: 'any maskable',
+      //       },
+      //     ],
+      //   },
+      // }),
+    ],
+
+    server: {
+      proxy: {
+        '/api': {
+          target: env.VITE_API_URL,
+          changeOrigin: true,
+          secure: false,
+        },
       },
     },
-  },
+  };
 });
