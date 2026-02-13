961060c06096159a6afab800d409cfb31b94121e
Yooonjeong <wkddbs3935@naver.com>
2026-02-14 02:36:42 +0900

    [BE] [FEAT] 활동 상태가 아닌 경우 에러 코드 정의

--- backend/src/main/java/org/sejongisc/backend/common/exception/ErrorCode.java
+++ backend/src/main/java/org/sejongisc/backend/common/exception/ErrorCode.java
@@ -89,6 +89,7 @@ public enum ErrorCode {
   DUPLICATE_PHONE(HttpStatus.CONFLICT, "이미 사용 중인 전화번호입니다."),
   DUPLICATE_USER(HttpStatus.CONFLICT, "이미 가입된 사용자입니다."),
   INVALID_INPUT(HttpStatus.BAD_REQUEST, "입력값이 올바르지 않습니다."),
+  USER_NOT_ACTIVE(HttpStatus.FORBIDDEN, "현재 활동 상태의 회원이 아닙니다."),
 
   // EXCEL
 
