98e645702365cefe25ac759bc81c75bed136b348
vmgfh878 <vmgfh878@gmail.com>
2026-03-19 15:41:47 +0900

    Delete test_patchtst.py

--- test_patchtst.py
+++
@@ -1,9 +0,0 @@
-import torch
-from AI.modules.signal.models.PatchTST.architecture import PatchTST_Model
-
-model = PatchTST_Model()
-x = torch.randn(2, 120, 17)
-out = model(x)
-print('입력:', x.shape)
-print('출력:', out.shape)
-print('성공!')
